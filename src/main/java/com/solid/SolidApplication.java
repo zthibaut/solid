88d62f705d299f7c142c031a941e132332812d7e
tibo <tibo@inclusivitysolutions.com>
2019-11-15 01:25:52 +0200

    Add linter

--- src/main/java/com/solid/SolidApplication.java
+++ src/main/java/com/solid/SolidApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SolidApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SolidApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SolidApplication.class, args);
+  }
 
 }
