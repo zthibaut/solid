789a4d9bca4dd8f4244f223120701bce10c49678
tibo <tibo@inclusivitysolutions.com>
2019-11-15 01:31:00 +0200

    Update JUnit

--- src/test/java/com/solid/SolidApplicationTests.java
+++ src/test/java/com/solid/SolidApplicationTests.java
@@ -3,11 +3,23 @@ package com.solid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 class SolidApplicationTests {
 
   @Test
-  void contextLoads() {
+  void shouldPass() {
+    assertThat(true, is(true));
+  }
+
+  @Test
+  void shouldThrowException() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      throw new IllegalArgumentException("Illegal happening here...");
+    });
   }
 
 }
